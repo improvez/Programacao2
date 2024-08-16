2c5b6dee05491cb541804a30c92efc57f4eab1be
Lucas Abati Zanotto <386188@unoesc.lan>
2024-08-15 21:01:07 -0300

    add: aula 15/08

---
+++ projeto/data_hora.php
@@ -0,0 +1,17 @@
+<?php
+// define o timezone
+date_default_timezone_set("America/Sao_Paulo");
+// escrever uma frase que mostre a data atual 
+echo "Hoje é dia " . date('F');
+echo " e agora são " . date('g A');
+
+/*
+    DESAFIOS
+    1. Na função date(), substitua Y por y e observe o resultado;
+    2. Pesquise com exibir a hora no formato 12 horas (A.M. e P.M.);
+    3. Exiba o nome do mês atual;
+
+    site oficial: php.net
+
+*/
+?>
---
+++ projeto/hello.php
@@ -0,0 +1,7 @@
+<?php
+// Isto é um comentário de uma linha 
+/*
+Isto é um comentário de várias linhas
+*/
+echo "Hello world!"
+?>
